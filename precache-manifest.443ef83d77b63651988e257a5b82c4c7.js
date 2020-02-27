a5aa3aac769496dab915cc0bb34c2ab0b72eea61
nidhaal12 <jemlinidhal3@gmail.com>
2020-02-27 16:18:14 +0100

    Updates

---
+++ precache-manifest.443ef83d77b63651988e257a5b82c4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f516bf36837abeeea2f7ecc7afa5d64",
+    "url": "/nav-bar-react/index.html"
+  },
+  {
+    "revision": "03fa089c1b0fb1141151",
+    "url": "/nav-bar-react/static/css/main.9c987095.chunk.css"
+  },
+  {
+    "revision": "a7948558d4dfbb6bf73b",
+    "url": "/nav-bar-react/static/js/2.df40e642.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nav-bar-react/static/js/2.df40e642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03fa089c1b0fb1141151",
+    "url": "/nav-bar-react/static/js/main.b9149d52.chunk.js"
+  },
+  {
+    "revision": "db9df7d58b493b6fa454",
+    "url": "/nav-bar-react/static/js/runtime-main.f602ec95.js"
+  }
+]);
